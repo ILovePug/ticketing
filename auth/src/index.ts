9c13840d03ddbd567bc39d309dc026c266e8f7dc
ILovePug <wengqians@yahoo.com>
2020-07-12 20:31:56 -0400

    done with error handling

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,13 +1,28 @@
 import express from 'express';
+// handles error throwing in async function so it wont hang
+import 'express-async-errors';
 import { json } from 'body-parser';
+import { currentUserRouter } from './routes/current-user';
+import { signinRouter } from './routes/siginin';
+import { signoutRouter } from './routes/signout';
+import { signupRouter } from './routes/signup';
+import { errorHandler } from './middlewares/error-handler'
+import { NotFoundError } from './errors/not-found-error'
 
 const app = express();
 app.use(json());
 
-app.get('/api/users/currentuser', (req, res) => {
-	res.send('hi there!!');
-});
+app.use(currentUserRouter);
+app.use(signinRouter);
+app.use(signoutRouter);
+app.use(signupRouter);
 
+app.all('*', async ()=>{
+	throw new NotFoundError();
+})
+
+
+app.use(errorHandler);
 app.listen(3000, () => {
 	console.log('listening on port 3000!!!');
 });
--- docs/useful-command.md
+++ docs/useful-command.md
@@ -1,2 +1,4 @@
 - redeploy same deployments `kubectl rollout restart deployment [deployment-name]`
-    
+
+- get list of contexts `kubectl config get-contexts`
+- remove a context `kubectl config delete-context [context-name]`
